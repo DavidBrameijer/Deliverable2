b57544c504ad0a96c737d28ecf26097af5726f73
DavidBrameijer <davidbrameijer@gmail.com>
2024-06-13 14:23:24 -0400

    deliv2

---
+++ Deliv2/Deliv2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to the Coin Flip Challenge?");
+        Console.WriteLine("What is your name?");
+
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name + " do you want to play the coinflip challenge? Yes or No");
+
+        string answer = Console.ReadLine().ToLower();
+        if (answer == "no" || answer == "n")
+        {
+            Console.WriteLine("You are a coward " + name + "!");
+        }
+
+        if (answer == "yes" || answer == "y")
+        {
+            int plays = 1;
+            int wins = 0;
+            while (plays <= 5)
+            {
+                Console.WriteLine("Heads or Tails?");
+                string coin = Console.ReadLine().ToLower();
+
+                Random random = new Random();
+                var n = random.Next(0, 2);
+                // 0 is heads and 1 is tails
+
+                string flipResult;
+                if (n == 0)
+                {
+                    flipResult = "heads";
+                }
+                else
+                {
+                    flipResult = "tails";
+                }
+
+
+                if (coin == flipResult)
+                {
+                    Console.WriteLine("Correct!");
+                    //Console.WriteLine(n);
+                    //Console.WriteLine(flipResult);
+                    wins++;
+                    plays++;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong!");
+                    //Console.WriteLine(n);
+                    //Console.WriteLine(flipResult);
+                    plays++;
+                }
+            }
+            Console.WriteLine("Thank you " + name + " you got a score of " + wins + "!");
+        }
+    }
+}
---
+++ Deliverable1/Deliverable1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to the restocking tool.");
+
+        int sodaStock = 100;
+        Console.WriteLine("How many Sodas have been sold today? 100 are in stock");
+        int sodaSold = int.Parse(Console.ReadLine());
+        int newSoda = sodaStock - sodaSold;
+
+        if (sodaSold > 100)
+        {
+            Console.WriteLine("That value is too high. Stock not adjusted.");
+        }
+        else
+        {
+            Console.WriteLine("There are " + newSoda + " sodas left");
+        }
+
+        int chipStock = 40;
+        Console.WriteLine("How many Chips have been sold today? 40 are in stock");
+        int chipSold = int.Parse(Console.ReadLine());
+        int newChip = chipStock - chipSold;
+
+        if (chipSold > 40)
+        {
+            Console.WriteLine("That value is too high. Stock not adjusted.");
+        }
+        else
+        {
+            Console.WriteLine("There are " + newChip + " chips left");
+        }
+
+        int candyStock = 60;
+        Console.WriteLine("How many Candy has been sold today? 60 are in stock");
+        int candySold = int.Parse(Console.ReadLine());
+        int newCandy = candyStock - candySold;
+
+        if (candySold > 60)
+        {
+            Console.WriteLine("That value is too high. Stock not adjusted.");
+        }
+        else
+        {
+            Console.WriteLine("There are " + newCandy + " Candy left");
+        }
+
+        Console.WriteLine("Thank you for filling out the values. Here's what needs to be restocked.");
+
+        if (newSoda <= 40)
+        {
+            Console.WriteLine("Soda needs to be restocked.");
+        }
+
+        if (newChip <= 20)
+        {
+            Console.WriteLine("Chips need to be restocked.");
+        }
+
+        if (newCandy <= 40)
+        {
+            Console.WriteLine("Candy needs to be restocked.");
+        }
+
+        Console.WriteLine("Goodbye!");
+    }
+}
